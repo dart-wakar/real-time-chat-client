48e340dd0de935fb7a34466a8530a90386b8932b
dart-wakar <wakar@whitelogic.in>
2017-07-02 02:39:46 +0530

    Simple messages send and display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {SocketIoModule,SocketIoConfig} from 'ng-socket-io';
 
 import {AppRoutingModule} from './app.routing';
 
 import { AppComponent } from './app.component';
 import {ChatRoomComponent} from './components/chat-room/chat-room.component';
+
+import {ChatRoomService} from './services/chat-room.service';
+
+const socketConfig: SocketIoConfig = {url: 'http://localhost:3000',options: {}};
 
 @NgModule({
   declarations: [
@@ -17,9 +22,12 @@ import {ChatRoomComponent} from './components/chat-room/chat-room.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(socketConfig)
   ],
-  providers: [],
+  providers: [
+    ChatRoomService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/chat-room/chat-room.component.ts
+++ src/app/components/chat-room/chat-room.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
+import {ChatRoomService} from '../../services/chat-room.service';
 
 @Component({
     selector: 'chat-room',
     templateUrl: './chat-room.component.html'
 })
 
-export class ChatRoomComponent {
+export class ChatRoomComponent implements OnInit {
+
     componentName = 'Chat room';
+    msg: string;
+
+    constructor(private chatRoomService: ChatRoomService) {}
+
+    ngOnInit() {
+        this.chatRoomService.getMessage()
+                            .subscribe(msg => {
+                                this.msg = "1st "+msg;
+                            });
+    }
+
+    sendMsg(msg) {
+        this.chatRoomService.sendMessage(msg);
+    }
+    
 }
