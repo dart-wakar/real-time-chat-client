0adc284f18aed06ff65d21a03d9958ccfdb04f5d
dart-wakar <wakar@whitelogic.in>
2017-07-15 05:15:29 +0530

    data structure to handle user online/offline status

---
+++ src/app/classes/user-status.handler.ts
@@ -0,0 +1,78 @@
+export class UserStatusHandler {
+
+    private onlineUsers: any = [];    
+    private offlineUsers: any = [];
+    private onlineUsersIds: any = [];
+    private offlineUsersIds: any = [];
+
+    constructor(onlineUsers,offlineUsers) {
+        let j: number = 0,k: number = 0;
+        for(j = 0;j < onlineUsers.length;j++) {
+            this.onlineUsersIds.push(onlineUsers[j]._id);
+            this.onlineUsers.push(onlineUsers[j]);
+        }
+        for(k = 0;k < this.offlineUsers.length;k++) {
+            this.offlineUsersIds.push(offlineUsers[k]._id);
+            this.offlineUsers.push(offlineUsers[k]);
+        }
+    }
+
+    public userCameOnline(user: any) {
+        this.addUserToOnlineList(user); //add only if not already present
+        this.removeUserFromOfflineList(user); //remove only if present
+    }
+
+    public userWentOffline(user: any) {
+        this.removeUserFromOnlineList(user); //remove only if present
+        this.addUserToOfflineList(user); //add only if not already present
+    }
+
+    public getOnlineUsers() {
+        return this.onlineUsers;
+    }
+
+    public getOfflineUsers() {
+        return this.offlineUsers;
+    }
+
+    private addUserToOnlineList(user: any) {
+        let i = this.onlineUsersIds.indexOf(user._id);
+        if(i === -1) {
+            this.onlineUsers.push(user);
+            this.onlineUsersIds.push(user._id);
+        }
+    }
+
+    private removeUserFromOfflineList(user: any) {
+        let i = this.offlineUsersIds.indexOf(user._id);
+        if(i > -1) {
+            this.offlineUsers.splice(i,1);
+            this.offlineUsersIds.splice(i,1);
+        }
+    }
+
+    private removeUserFromOnlineList(user: any) {
+        let i = this.onlineUsersIds.indexOf(user._id);
+        if(i > -1) {
+            this.onlineUsers.splice(i,1);
+            this.onlineUsersIds.splice(i,1);
+        }
+    }
+
+    private addUserToOfflineList(user: any) {
+        let i = this.offlineUsersIds.indexOf(user._id);
+        if(i === -1) {
+            this.offlineUsers.push(user);
+            this.offlineUsersIds.push(user._id);
+        }
+    }
+
+    public getOnlineUsersIds() {
+        return this.onlineUsersIds;
+    }
+
+    public getOfflineUsersIds() {
+        return this.offlineUsersIds;
+    }
+
+}
--- src/app/components/user-status/user-status.component.ts
+++ src/app/components/user-status/user-status.component.ts
@@ -1,16 +1,18 @@
-import {Component,OnInit} from '@angular/core';
+import {Component,OnInit,OnChanges} from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {ChatRoomService} from '../../services/chat-room.service';
+import {UserStatusHandler} from '../../classes/user-status.handler';
 
 @Component({
     selector: 'user-status',
     templateUrl: './user-status.component.html'
 })
 
-export class UserStatusComponent implements OnInit {
+export class UserStatusComponent implements OnInit,OnChanges {
 
     onlineUsers: any;
     offlineUsers: any;
+    userStatusHandler: UserStatusHandler;
 
     constructor(private userService: UserService,private chatRoomService: ChatRoomService) {}
 
@@ -26,17 +28,36 @@ export class UserStatusComponent implements OnInit {
             .subscribe(offlineUsers => {
                 console.log(offlineUsers);
                 this.offlineUsers = offlineUsers;
+                this.userStatusHandler = new UserStatusHandler(this.onlineUsers,this.offlineUsers);
             });
         this.chatRoomService.getNewUserConnection()
-            .subscribe(data => {
-                console.log(data);
-                this.onlineUsers.push(data.user);
-            });
-        this.chatRoomService.getUserOffline()
-            .subscribe(data => {
-                console.log(data);
-                this.offlineUsers.push(data.user);
-            })
+                .subscribe(data => {
+                    if(this.userStatusHandler !== undefined) {
+                        console.log(data);
+                        this.onlineUsers.push(data.user);
+                        this.userStatusHandler.userCameOnline(data.user);
+                        console.log(this.userStatusHandler.getOfflineUsers());
+                        console.log(this.userStatusHandler.getOfflineUsersIds());
+                        console.log(this.userStatusHandler.getOnlineUsers());
+                        console.log(this.userStatusHandler.getOnlineUsersIds());
+                    }                    
+                });
+            this.chatRoomService.getUserOffline()
+                .subscribe(data => {
+                    if(this.userStatusHandler !== undefined) {
+                        console.log(data);
+                        this.offlineUsers.push(data.user);
+                        this.userStatusHandler.userWentOffline(data.user);
+                        console.log(this.userStatusHandler.getOfflineUsers());
+                        console.log(this.userStatusHandler.getOfflineUsersIds());
+                        console.log(this.userStatusHandler.getOnlineUsers());
+                        console.log(this.userStatusHandler.getOnlineUsersIds());
+                    }
+                });
+    }
+
+    ngOnChanges() {
+
     }
 
 }
